a54a17b253f24ca975c8d1bac992adcdbbf0db98
erik2282008 <58351128+erik2282008@users.noreply.github.com>
2026-01-10 00:04:35 +0700

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,21 +1,45 @@
-import api from "./api.js";
 import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+
 import { initDB } from "./db.js";
-
-await initDB();
-
 import "./bot.js";
 import "./watcher.js";
 import "./post_end_watcher.js";
+import api from "./api.js";
 
+/* ================== PATH FIX ================== */
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+/* ================== INIT DB ================== */
+await initDB();
+
+/* ================== EXPRESS ================== */
 const app = express();
 
 app.use(express.json());
-app.use(express.static("frontend"));
 
+/* ================== API ================== */
+app.use("/api", api);
+
+/* ================== FRONTEND ================== */
+app.use(
+  express.static(
+    path.join(__dirname, "../frontend")
+  )
+);
+
+/* ================== ROOT ================== */
+app.get("/", (req, res) => {
+  res.sendFile(
+    path.join(__dirname, "../frontend/index.html")
+  );
+});
+
+/* ================== START ================== */
 const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
   console.log("COUNTDOWN SERVER RUNNING");
 });
-
